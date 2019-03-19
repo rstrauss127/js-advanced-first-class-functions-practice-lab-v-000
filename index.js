5f5795e126155d0c341c83de47591c8a1eaf308f
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-19 19:10:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (person) {
+    console.log(person.name);
+  }) ;
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function (person) {
+    if (person.hometown == location) {
+      console.log(person.name);
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort(function (a, b) {
+    return a.revenue-b.revenue;
+  });
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(drivers) {
+  var initialValue = 0;
+  return drivers.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue.revenue;
+  }, initialValue)
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers)/drivers.length;
+}
