335aab2e0cbd1f78c6aa4f8385ab0b4d2f1b5c66
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-19 18:59:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function logDriverNames(drivers) {
-  drivers.forEach(function(person) {
+  drivers.forEach(function (person) {
     console.log(person.name);
   }) ;
 }
 
 function logDriversByHometown(drivers, location) {
-  drivers.forEach(function(person) {
+  drivers.forEach(function (person) {
     if (person.hometown == location) {
       console.log(person.name);
     }
@@ -15,5 +15,11 @@ function logDriversByHometown(drivers, location) {
 function driversByRevenue(drivers) {
   return drivers.slice().sort(function (a, b) {
     return a.revenue-b.revenue;
+  })
+}
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function (a, b) {
+    return a.name - b.name;
   })
 }
