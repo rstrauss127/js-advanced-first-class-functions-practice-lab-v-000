1be4ddbf1fb8e1f41bb930809545d59f5da53d0d
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-19 18:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// Code your solution in this file!
 function logDriverNames(drivers) {
   drivers.forEach(function(person) {
     console.log(person.name);
   }) ;
+}
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function(person) {
+    if (person.hometown == location) {
+      console.log(person.name);
+    }
+  });
 }
